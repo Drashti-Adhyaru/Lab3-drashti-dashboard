0586bfaf0b468e0a9d5bd0739ae8bfe0844d151b
Drashti-Adhyaru <n01539964@humbermail.ca>
2024-01-31 22:46:25 -0500

    added

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -20,6 +20,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'try', href: '/dashboard/try', icon: UserGroupIcon }
 ];
 
 export default function NavLinks() { 
